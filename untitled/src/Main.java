5066aa60084b1c5ba77917cf6b193f1affb36a09
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-05-18 15:55:45 +0300

    created new homework

---
+++ untitled/src/Main.java
@@ -0,0 +1,123 @@
+// Формат сдачи: ссылка на подписанный git-проект.
+
+// Задание
+
+// 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! 
+// (произведение чисел от 1 до n)
+// 2) Вывести все простые числа от 1 до 1000
+// 3) Реализовать простой калькулятор
+// 4) (дополнительное задание) Задано уравнение вида q + w = e, q, w, e >= 0. 
+// Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69. 
+// Требуется восстановить выражение до верного равенства. 
+// Предложить хотя бы одно решение или сообщить, что его нет.
+
+import java.util.ArrayList;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Введите число, факториал которого нужно найти: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//        scanner.close();
+//        int a = 1;
+//        for (int i = 2; i <= n; i++) {
+//            a = a * i;
+//        }
+//        System.out.println(a);
+
+// 2) Вывести все простые числа от 1 до 1000
+
+//       int a = 2;
+//       int n = 1000;
+//       //int count = 0;
+//        for (int i = 2; i < n; i++) {
+//            if (a % a == 0){
+//                System.out.println(i);
+//            a = a + 6;
+            //count = count +1;
+
+//int count = 4;
+//int n = 1000;
+//ArrayList<Integer> numbers = new ArrayList<>();
+//numbers.add(2);
+//numbers.add(3);
+//numbers.add(5);
+//numbers.add(7);
+//        for (int i = 11; i <= n; i+=2) {
+//            if (simple(i)) {
+//                count++;
+//                numbers.add(i);
+//            }
+//        }
+//
+//        System.out.println("Количество простых чисел: " + count);
+//       System.out.print(numbers);
+//
+//    }
+//
+//    static boolean simple(int a) {
+//        int p = 0;
+//        if ((a % 2 == 0)||(a%10==5)) {
+//            return false;
+//        }
+//        else {
+//            for (int j = 3; j <= Math.sqrt(a); j += 2) {
+//                if (a % j == 0) {
+//                    p += 1; break;
+//                }
+//            }
+//        }
+//        if (p > 0) {
+//            return false;
+//        } else return true;
+
+
+// 3) Реализовать простой калькулятор
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int answer;
+//        System.out.println("Выберите действие:");
+//        char operator = scanner.next().charAt(0);
+//        System.out.println("Введите первое число:");
+//        int a = scanner.nextInt();
+//        System.out.println("Введите второе число:");
+//        int b = scanner.nextInt();
+//        scanner.close();
+//
+//        switch (operator) {
+//            case '+':
+//                answer = a + b;
+//                System.out.println("Сумма ваших чисел: " + answer);
+//                break;
+//            case '-':
+//                answer = a - b;
+//                System.out.println("Разность ваших чисел: " + answer);
+//                break;
+//            case '*':
+//                answer = a * b;
+//                System.out.println("Произведение ваших чисел: " + answer);
+//                break;
+//            case '/':
+//                answer = a / b;
+//                System.out.println("Частное ваших чисел: " + answer);
+//                break;
+//        }
+
+// 4) (дополнительное задание) Задано уравнение вида q + w = e, q, w, e >= 0.
+// Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69.
+// Требуется восстановить выражение до верного равенства.
+// Предложить хотя бы одно решение или сообщить, что его нет.
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
